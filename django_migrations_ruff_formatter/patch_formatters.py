6b16696fcee1d7e1f056eb42b5329994b60b0167
Jonas Vacek <jvacek@users.noreply.github.com>
2025-05-05 21:18:18 +0200

    remove loose print, attribution

--- django_migrations_ruff_formatter/patch_formatters.py
+++ django_migrations_ruff_formatter/patch_formatters.py
@@ -5,6 +5,8 @@ patched to use ruff for formatting Django generated files, instead of black.
 These patches are applied when they are imported from core.__init__ during the Django startup.
 """
 
+# Loosely inspired by https://github.com/ulgens/django-official-tutorial/blob/main/src/core/patch_formatters.py
+
 import shutil
 import subprocess  # noqa: S404
 import sys
@@ -39,7 +41,6 @@ def run_formatters(written_files, ruff_path=(sentinel := object()), stderr=sys.s
         format_args = [ruff_path, "format", *extra_format_args, *written_files]
         try:
             subprocess.run(lint_args, capture_output=True)
-            print(f"{format_args=}")
             subprocess.run(format_args, capture_output=True)
         except OSError:
             stderr.write("Formatters failed to launch:")
