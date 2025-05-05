46ba357d42e15539358bbb15d90408e604d24645
Jonas Vacek <jvacek@users.noreply.github.com>
2025-05-05 21:08:54 +0200

    suuport extra args in settings, remove force exclude by default

--- django_migrations_ruff_formatter/patch_formatters.py
+++ django_migrations_ruff_formatter/patch_formatters.py
@@ -7,7 +7,10 @@ These patches are applied when they are imported from core.__init__ during the D
 
 import shutil
 import subprocess  # noqa: S404
+import sys
+import traceback
 
+from django.conf import settings
 from django.core.management import utils
 
 
@@ -21,7 +24,7 @@ def find_formatters() -> dict:
 utils.find_formatters = find_formatters
 
 
-def run_formatters(written_files, ruff_path=(sentinel := object())):  # noqa: B008
+def run_formatters(written_files, ruff_path=(sentinel := object()), stderr=sys.stderr):  # noqa: B008
     """
     Run ruff on the given files.
     """
@@ -30,8 +33,17 @@ def run_formatters(written_files, ruff_path=(sentinel := object())):  # noqa: B0
         ruff_path = shutil.which("ruff")
 
     if ruff_path:
-        subprocess.run([ruff_path, "check", "--force-exclude", "--fix", *written_files], capture_output=True)
-        subprocess.run([ruff_path, "format", "--force-exclude", *written_files], capture_output=True)
+        extra_lint_args = getattr(settings, "RUFF_EXTRA_LINT_ARGS", [])
+        lint_args = [ruff_path, "check", "--fix", *extra_lint_args, *written_files]
+        extra_format_args = getattr(settings, "RUFF_EXTRA_FORMAT_ARGS", [])
+        format_args = [ruff_path, "format", *extra_format_args, *written_files]
+        try:
+            subprocess.run(lint_args, capture_output=True)
+            print(f"{format_args=}")
+            subprocess.run(format_args, capture_output=True)
+        except OSError:
+            stderr.write("Formatters failed to launch:")
+            traceback.print_exc(file=stderr)
 
 
 utils.run_formatters = run_formatters
